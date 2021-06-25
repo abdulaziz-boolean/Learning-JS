41727b619b1cb275848c85db75806398c84e21cf
abdulaziz <abdulaziz.boolean@gmail.com>
2021-06-25 21:33:26 +0800

    setup project

---
+++ MakeObject/index.js
@@ -0,0 +1,17 @@
+let Student = {
+  name: "Abdul Aziz",
+  age: 22,
+  hoby: function (hobbies) {
+    console.log(`my hobi ${hobbies}`);
+  },
+};
+let Student2 = {
+  name: "Azizah",
+  age: 20,
+  hoby: function (hobbies) {
+    console.log(`my hobi ${hobbies}`);
+  },
+};
+
+console.log(Student.hoby("coding"));
+console.log(Student2.hoby("design"));
